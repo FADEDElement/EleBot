d5e302aa1b519c6ad52aca33e3be2e28cc3d38b6
Thomas Henderson <34247897+FADEDElement@users.noreply.github.com>
2023-01-08 09:31:20 +0000

    added main file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "!", intents = discord.Intents.all())
+
+@client.command()
+async def test(ctx):
+  await ctx.send("I am not a robot!")
+
+client.run(os.environ['discordToken'])
